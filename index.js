98dc9560a36b7b6757090f79d3eb9f7f4e51edb1
Norman Smart <normansmart14@gmail.com>
2022-05-18 11:16:00 -0400

    commit tree abb1a5d2a307fdff8481d678ec02c1565f9e4839

--- index.js
+++ index.js
@@ -1,1 +1,89 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+
+function destructivelyAppendCat(array){
+
+    
+
+    cats.push('Ralph');
+
+    return cats;
+}
+
+
+function destructivelyPrependCat(array){
+
+    
+
+    cats.unshift('Bob');
+
+    return cats;
+}
+
+
+function destructivelyRemoveLastCat(array){
+
+    
+
+    cats.pop('Ralph');
+
+    return cats;
+}
+
+
+
+function destructivelyRemoveFirstCat(array){
+
+    
+
+    cats.shift('Ralph');
+
+    return cats;
+}
+
+
+
+function appendCat(array){
+
+    const broom = cats.slice();
+
+    broom.push('Broom');
+
+    return broom;
+}
+
+
+
+
+function prependCat(array){
+
+    const Arnold = cats.slice();
+
+    Arnold.unshift('Arnold');
+
+    return Arnold;
+}
+
+
+
+function removeLastCat(array){
+
+    const removeLastCat = cats.slice(0, cats.length - 1);
+
+
+  return removeLastCat;
+
+}
+
+
+
+
+function removeFirstCat(array){
+
+    const removeFirstCat = cats.slice(1);
+
+
+  return removeFirstCat;
+
+}
